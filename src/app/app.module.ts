d8cc8e6a11315b6eca19acb3c8d1e9929d2e4725
skg1998 <sahilkg1998@gmail.com>
2020-05-01 17:39:33 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,NgxDropzoneModule ],
+  imports:      [ BrowserModule, FormsModule ,NgxDropzoneModule ,FontAwesomeModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
