4f11c0c76a24dbe85e6f59fa8a8cad4b2cb87344
skg1998 <sahilkg1998@gmail.com>
2020-05-05 14:42:09 +0530

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { HelloComponent } from './hello.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { RestApiComponent } from './rest-api/rest-api.component';
 import { StepperComponent } from './stepper/stepper.component';
+import { SelectPropertiesComponent } from './select-properties/select-properties.component';
+import { DownloadComponent } from './download/download.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,NgxDropzoneModule ,FontAwesomeModule,],
-  declarations: [ AppComponent, HelloComponent, FileUploadComponent, RestApiComponent, StepperComponent ],
+  declarations: [ AppComponent, HelloComponent, FileUploadComponent, RestApiComponent, StepperComponent, SelectPropertiesComponent, DownloadComponent, HomePageComponent ],
   bootstrap:    [ AppComponent ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
---
+++ src/app/select-properties/select-properties.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-properties',
+  templateUrl: './select-properties.component.html',
+  styleUrls: ['./select-properties.component.css']
+})
+export class SelectPropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
